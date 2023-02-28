1b0363badb6897abb75f18e7cf09413b5d1102ed
O-Isaac <zaragozamendozaisaac@gmail.com>
2023-02-28 21:16:08 +0100

    fix(decompile): future issue #2

--- libs/python/decompile.py
+++ libs/python/decompile.py
@@ -15,8 +15,15 @@ def decompile_apk():
 def decrypt():
     il2cpp = os.path.join(os.getcwd(), "libs", "Il2CppDumper", "Il2CppDumper-x86.exe")
     global_metadata = os.path.join(config.temp_folder, "files", "assets", "bin", "Data", "Managed", "Metadata", "global-metadata.dat")
-    libil2cpp = os.path.join(config.temp_folder, "files", "lib", "arm64-v8a", "libil2cpp.so")
     decrypt = os.path.join(config.temp_folder, "decrypt");
+
+    print("[App] Getting available libraries directories from apk")
+    lib_folders = os.listdir(config.temp_folder, "files", "lib")
+
+    print(f"[App] ArchLibs avalibles {lib_folders}")
+    print(f"[App] Getting libil2cpp from {lib_folders[0]}")
+    lib_dir_name = lib_folders[0]
+    libil2cpp = os.path.join(config.temp_folder, "files", "lib", lib_dir_name, "libil2cpp.so")
 
     print('[App] Decrypting Files!', file=sys.stdout)
     p = subprocess.Popen(f"{il2cpp} {libil2cpp} {global_metadata} {decrypt}", stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True);
