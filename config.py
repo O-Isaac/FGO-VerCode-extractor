094c2f2d645c325ca445854ce79684ce839c9d27
Isaac <zaragozamendozaisaac@outlook.es>
2024-06-26 18:58:33 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os;
 
 # https://fgo.square.ovh/apk/com.aniplex.fategrandorder.en.apk
-url_apk = "https://fgo.square.ovh/apk/com.aniplex.fategrandorder.apk"
+url_apk = "https://fgo.square.ovh/apk/com.aniplex.fategrandorder.en.apk"
 
 # https://gplay-ver.atlasacademy.workers.dev/?id=com.aniplex.fategrandorder
-url_version = "https://gplay-ver.atlasacademy.workers.dev/?id=com.aniplex.fategrandorder"
+url_version = "https://gplay-ver.atlasacademy.workers.dev/?id=com.aniplex.fategrandorder.en"
 
 # Temp folder
 temp_folder = os.path.join(os.getcwd(), "temp")
